f9331165b336845049cc364c56ac7a0ae2e713db
AliSalih1995git <kvaliswalihtnl@gmail.com>
2022-12-15 19:20:37 +0530

    backend cors

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const { readdirSync } = require('fs');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const { readdirSync } = require("fs");
+const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 app.use(express.json());
-
-app.use(cors());
+const options = {
+  origin: "*",
+};
+app.use(cors(options));
 app.use(
   fileUpload({
     useTempFiles: true,
   })
 );
 //Routes
-readdirSync('./routes').map((r) => app.use('/', require('./routes/' + r)));
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
 //Datebase
 mongoose
   .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
   })
-  .then(() => console.log('Database connected successfully'))
-  .catch((err) => console.log('Error connecting to mongodb', err));
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log("Error connecting to mongodb", err));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
