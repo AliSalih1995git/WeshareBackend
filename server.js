febc23acf5162ed1dc5901fae7b4f22575972489
AliSalih1995git <kvaliswalihtnl@gmail.com>
2022-12-16 12:07:00 +0530

    cors 16

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ const app = express();
 // };
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://main.d28fqyt2gxwcsx.amplifyapp.com"
-  );
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Methods",
     "GET,POST,PUT,PATCH,DELETE,UPDATE,OPTIONS"
@@ -32,7 +29,7 @@ app.use(
   cors({
     allowedHeaders: "*",
     allowMethods: "*",
-    origin: "https://main.d28fqyt2gxwcsx.amplifyapp.com",
+    origin: "http://localhost:3000",
   })
 );
 app.use(
