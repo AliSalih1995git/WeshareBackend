e45fd6a2669a9236f32eb3af9b4284d40121d060
AliSalih1995git <kvaliswalihtnl@gmail.com>
2022-12-16 14:52:16 +0530

    revert cors

--- server.js
+++ server.js
@@ -11,25 +11,30 @@ const app = express();
 //   origin: "http://localhost:3000",
 //   optionsSuccessStatus: 200,
 // };
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://main.d28fqyt2gxwcsx.amplifyapp.com/"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "GET,POST,PUT,PATCH,DELETE,UPDATE,OPTIONS"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With, X-HTTP-Method-Override,Content-Type,Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://main.d28fqyt2gxwcsx.amplifyapp.com"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,UPDATE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override,Content-Type,Accept"
+  );
+  next();
+});
 
 app.use(
-  cors('*');
+  cors({
+    allowedHeaders: "*",
+    allowMethods: "*",
+    origin: "*",
+  })
+);
 app.use(
   fileUpload({
     useTempFiles: true,
